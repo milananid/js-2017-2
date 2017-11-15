3a2ad327203d871c9086b2e53b55d4db88fecd2d
Nid <milana.nid@gmail.com>
2017-11-15 22:21:39 +0100

    non blocking

---
+++ 01 - installare jquery/non-blocking/js/main.js
@@ -0,0 +1,29 @@
+;(function (){
+  'use strict' //https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/Strict_mode
+
+  console.log($('html').html());
+
+  $('body').append('<a href="#">Niente</a>');
+
+  $('a').html('just a link');
+
+  $('a').goGreen();
+
+  var $tit = "un titolo";
+  var $link = "link.html";
+
+  $( "a" ).attr({
+      title: $tit,
+      href: $link
+  });
+
+}())
+
+
+$( document ).ready(function() {
+  console.log( "document loaded" );
+});
+
+$( window ).on( "load", function() {
+  console.log( "window loaded" );
+});
