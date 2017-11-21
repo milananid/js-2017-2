9a0f7542889c268ce7d1281589b2422967df578e
Nid <milana.nid@gmail.com>
2017-11-21 22:05:33 +0100

    esercizi 001

---
+++ 02 - Esercizi/001/js/main.js
@@ -0,0 +1,47 @@
+;(function (){
+  'use strict'
+
+  /*
+  ESERCIZIO 1
+  selezionare l'elemento h2 dell'header di pagina e printarlo nella console
+  con il comando console.log();
+  */
+
+  /*
+  ESERCIZIO 2
+  selezionare l'elemento a dell'header di pagina. Leggerne il parametro href
+   e printarlo nella console con il comando console.log();
+  */
+
+  /*
+  ESERCIZIO 3
+  compilare il link nell'header di pagina aggiungedo l'attributo title=""
+  con il valore dell'h2 dell'header di pagina. Nel medesimo elemento aggiungere
+  la classe "btn"
+  */
+
+  /*
+  ESERCIZIO 4
+  per ogli elemento article modificare la proprietà css background-color in #333
+  */
+
+  /*
+  ESERCIZIO 5
+  per ogli elemento article inserire un attributo data-index progressivo a partire da 0 e rimuovere
+  le classi left e right
+  */
+
+  /*
+  ESERCIZIO 6
+  selezionare l'elemento con id #head ed aggiungerci la classe big
+  */
+
+  /*
+  ESERCIZIO 7
+  selezionare gli elementi con attributo data-index == 1 e settarne il css width
+  a 75%; e margin-left 25%
+  selezionare il quarto elemento con attributo data-index e settarne il css width
+  a 75%; e margin-right 25%
+  */
+
+}())
